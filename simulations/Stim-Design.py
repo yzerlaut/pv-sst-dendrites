db4327eae96c8c3612833ec0cc3aa6f06ee1949a
Yann Zerlaut <yann.zerlaut@gmail.com>
2023-03-06 18:05:36 +0100

    update stim branch

--- simulations/Stim-Design.py
+++ simulations/Stim-Design.py
@@ -44,10 +44,31 @@ ax.set_ylabel('density')
 ax.set_yticks([]);
 
 # %% [markdown]
-# ## Distribute synapses
+# ## Distribute synapses on a single dendritic branch
 
 # %%
-x = np.linspace(SEGMENTS['distance_to_soma'].min(), SEGMENTS['distance_to_soma'].max(), 25)
+# select a given dendrite, the longest one !
+iEndDendrite = np.argmax(SEGMENTS['distance_to_soma'])
+SETS, i = [SEGMENTS['name'][iEndDendrite]], 0
+while (i<10) and len(SETS[-1].split('.'))>1:
+    new_name =  '.'.join(SETS[-1].split('.')[:-1])
+    SETS.append(new_name)
+    i+=1
+BRANCH_LOCS = []
+for i, name in enumerate(SEGMENTS['name']):
+    if name in SETS:
+        BRANCH_LOCS.append(i)
+
+fig, ax = pt.plt.subplots(1, figsize=(2,2))
+vis.plot_segments(ax=ax, color='tab:grey')
+vis.add_dots(ax, BRANCH_LOCS, 2)
+ax.set_title('n=%i segments' % len(SEGMENTS['name']), fontsize=6)
+BRANCH_LOCS = np.array(BRANCH_LOCS, dtype=int)
+
+
+# %%
+x = np.linspace(SEGMENTS['distance_to_soma'][BRANCH_LOCS].min(),
+                SEGMENTS['distance_to_soma'][BRANCH_LOCS].max(), 25)
 uniform = 0.5 +0*x
 uniform /= np.sum(uniform) #np.trapz(uniform, x=1e6*x)
 
@@ -55,12 +76,12 @@ biased = 1.-(x-x.min())/(x.max()-x.min())
 biased /= np.sum(biased) # np.trapz(biased, x=1e6*x)
 
 # %%
-Nsynapses = 100
+Nsynapses = 50
 
 np.random.seed(20)
 LOCS = {}
 
-digitized_dist = np.digitize(SEGMENTS['distance_to_soma'], bins=x, right=True)
+digitized_dist = np.digitize(SEGMENTS['distance_to_soma'][BRANCH_LOCS], bins=x, right=True)
 
 for case, proba in zip(['uniform', 'biased'], [uniform, biased]):
     
@@ -69,7 +90,9 @@ for case, proba in zip(['uniform', 'biased'], [uniform, biased]):
     
     for i in iDist:
         # pick one segment loc for the synapse with the distance condition:
-        LOCS[case].append(np.random.choice(np.arange(len(SEGMENTS['x']))[digitized_dist==i], 1)[0])
+        LOCS[case].append(\
+            np.random.choice(BRANCH_LOCS[digitized_dist==i], 1)[0])
+LOCS['single-syn'] = BRANCH_LOCS # to have them all
 
 
 # %%
@@ -98,11 +121,18 @@ for c, y, case in zip(range(2), [uniform, biased], ['uniform', 'biased']):
 # %% [markdown]
 # ## Synaptic integration with "uniform" and "biased" distributions
 
+# %% [markdown]
+# ## Equation and Parameters
+
 # %%
-gL = 5e-5*nrn.siemens/nrn.cm**2
+# passive
+gL = 1e-4*nrn.siemens/nrn.cm**2
 EL = -70*nrn.mV                
 Es = 0*nrn.mV                  
-
+# synaptic
+taus = 5.*nrn.ms
+w = 1.*nrn.nS
+# equation
 eqs='''
 Im = gL * (EL - v) : amp/meter**2
 Is = gs * (Es - v) : amp (point current)
@@ -112,19 +142,17 @@ gs : siemens
 # %%
 results = {}
 
-Nstim = 10
-results['events'] = np.arange(Nstim)*200
-results['Nsyns'] = 1+np.arange(Nstim)*10
+Nstim, Nrepeat, interstim = 10, 2, 200
+results['events'] = np.arange(Nstim)*interstim
+results['Nsyns'] = 1+np.arange(Nstim)*5
 
-for case in ['uniform', 'biased']:
+for case in ['single-syn-uniform', 'uniform', 'biased', 'single-syn-biased']:
 
     results[case] = {'Vm':[]}
     
-    for repeat in range(2):
+    for repeat in range(Nrepeat):
         
         np.random.seed(repeat)
-
-        spike_IDs, spike_times, synapses = np.empty(0, dtype=int), np.empty(0), np.empty(0, dtype=int)
 
         neuron = nrn.SpatialNeuron(morphology=morpho, 
                            model=eqs,
@@ -132,15 +160,18 @@ for case in ['uniform', 'biased']:
                            Ri=100 * nrn.ohm * nrn.cm)
         neuron.v = EL
 
-        taus = 5.*nrn.ms
-        w = 0.2*nrn.nS
+        if 'single-syn' in case:
+            spike_times = np.arange(Nsynapses)*interstim
+            spike_IDs = np.arange(Nsynapses)
+        else:
+            spike_IDs, spike_times, synapses = np.empty(0, dtype=int), np.empty(0), np.empty(0, dtype=int)
+            for e, ns in zip(results['events'], results['Nsyns']):
+                s = np.random.choice(np.arange(Nsynapses), ns, replace=False)
+                spike_times = np.concatenate([spike_times, e+np.arange(len(s))*0.01])
+                spike_IDs = np.concatenate([spike_IDs, np.array(s, dtype=int)])
 
-        for e, ns in zip(results['events'], results['Nsyns']):
-
-            s = np.random.choice(np.arange(Nsynapses), ns, replace=False)
-
-            spike_times = np.concatenate([spike_times, e+np.arange(len(s))*0.01])
-            spike_IDs = np.concatenate([spike_IDs, np.array(s, dtype=int)])
+        results[case]['spike_times_%i'%repeat] = spike_times
+        results[case]['spike_IDs_%i'%repeat] = spike_IDs
 
         stimulation = nrn.SpikeGeneratorGroup(Nsynapses,
                                               np.array(spike_IDs, dtype=int),
@@ -152,15 +183,34 @@ for case in ['uniform', 'biased']:
                            on_pre='g += w',
                            method='exponential_euler')
 
-        for ipre, iseg_post in enumerate(LOCS[case]): # connect spike IDs to a given location
+        for ipre, iseg_post in enumerate(LOCS[case.replace('single-syn-', '')]): # connect spike IDs to a given location
             ES.connect(i=ipre, j=iseg_post)
 
         # recording and running
-        M = nrn.StateMonitor(neuron, ('v'), record=[0, 1000])
+        M = nrn.StateMonitor(neuron, ('v'), record=[0])
         nrn.run((400+np.max(spike_times))*nrn.ms)
         results[case]['Vm'].append(np.array(M.v[0]/nrn.mV))
         
-results['t'] = np.array(M.t/nrn.ms)
+        if ('t' not in results) and ('single-syn' not in case):
+            results['t'] = np.array(M.t/nrn.ms)
+
+# %%
+np.save('results.npy', results)
+
+# %%
+results = np.load('results.npy', allow_pickle=True).item()
+
+# %%
+# build linear prediction from single-syn
+def build_linear_pred_trace(results):
+    
+    for case in ['uniform']:
+        for iLoc in np.unique(LOCS[case]):
+            iEvent = np.flatnonzero(LOCS[case]==)
+            print(iLoc)
+    results['single-syn'] = LOCS[case][iLoc]
+
+
 
 # %%
 for case in ['uniform', 'biased']:
@@ -211,6 +261,7 @@ for i, name in enumerate(SEGMENTS['name']):
 fig, ax = pt.plt.subplots(1, figsize=(2,2))
 vis.plot_segments(ax=ax, color='tab:grey')
 vis.add_dots(ax, BRANCH_LOCS, 2)
+BRANCH_LOCS = np.array(BRANCH_LOCS, dtype=int)
 
 
 # %%
