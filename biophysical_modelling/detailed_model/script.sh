e5a6c208dbbc8ae0019d687787f1b2cad8bc6e73
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2023-12-07 16:40:19 +0100

    added broadening script

--- biophysical_modelling/detailed_model/script.sh
+++ biophysical_modelling/detailed_model/script.sh
@@ -99,7 +99,7 @@ fi
 
 if [[ $1 == 'all' || $1 == 'broadening-full' ]]
 then
-    python intensity_timing_sim.py --freq 1e-2 --width 6.25 --broadening 1 2 4 8 16 --bgStimFreq 5e-4 --bgFreqInhFactor 4 -c Martinotti --test_NMDA --nBranch 6 --nStimRepeat 50 --ISI 500 --suffix BroadeningExample
+    python intensity_timing_sim.py --freq 1e-2 --width 6.25 --broadening 1 2 4 8 16 --bgStimFreq 5e-4 --bgFreqInhFactor 4 -c Martinotti --test_NMDA --nBranch 6 --nStimRepeat 50 --ISI 500 --suffix BroadeningFull
 fi
 
 
